c709b6b9bbbf7f4368e69985db11a692a449cb96
Mohamed Saleh <mohamed20163858@gmail.com>
2025-01-17 23:35:12 +0200

    Test app

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,14 @@
 import { SignOut } from "@/components/Signout";
+import { auth } from "@/auth";
 
-const Home = () => {
+const Home = async () => {
+  const session = await auth();
+  const user = session?.user;
   return (
     <div>
       <p>Home page</p>
+      {user && <p>Welcome, {user.name}</p>}
+      {user && <p>your email is, {user.email}</p>}
       <SignOut />
     </div>
   );
